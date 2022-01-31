6df8497a7c54f4083efd9f2afdabffb1b3889c37
pablo.guelu <pablo.guelu@gmail.com>
2022-01-31 17:50:27 +0100

    tinymce editor added to postJob view

---
+++ resources/js/components/tinyMCE.vue
@@ -0,0 +1,40 @@
+ <template>
+  <div id="app">
+    <editor
+      :api-key="api_key"
+      :init="{
+        height: 300,
+        menubar: false,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount',
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist outdent indent | removeformat | help',
+      }"
+    />
+  </div>
+</template>
+
+ <script>
+ import Editor from '@tinymce/tinymce-vue';
+
+ export default {
+   
+   name: 'app',
+   components: {
+     'editor': Editor
+   },
+
+   data () {
+       return {
+           api_key: process.env.MIX_TINYKEY
+       }
+   },
+
+   
+ }
+ </script>
